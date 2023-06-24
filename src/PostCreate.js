c42a22ccf7474b436305bca88561e69256b07797
cesar <cesar@everst.work>
2023-06-23 19:44:49 -0500

    first commit

---
+++ src/PostCreate.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export const PostCreate = () => {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      await axios.post('http://posts.com:4000/posts/create', {
+        title
+      });
+      setTitle('');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label>Title</label>
+          <input
+            className='form-control'
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </div>
+        <button className='btn btn-primary'>Submit</button>
+      </form>
+    </div>
+  )
+}
